48a0dda9b6700d9e828b3849a4189d05ae2cda71
Lena Ptak <lena.anastazja.ptak@gmail.com>
2026-03-06 21:15:54 +0100

    feat: gallery with real photos

--- app/src/components/GallerySection.jsx
+++ app/src/components/GallerySection.jsx
@@ -1,20 +1,20 @@
 import { useTranslation } from 'react-i18next'
 
+const zewnFiles = ['11.jpg', '20251019_153013.jpg', '22.png', 'DJI_0520.JPG', 'DSC09468.JPG', 'IMG-20251021-WA0011.jpg', 'jesien.jpg', 'kopula.jpg', 'palac_dawniej.jpg', 'pionowa_zima.jpg', 'pionowe.jpg', 'pocztowki.jpg', 'zima.jpg']
+const parkFiles = ['20251019_140721.jpg', '20251019_155511.jpg', '20251019_161301.jpg', '20251019_161848.jpg', 'IMG_0345.jpg', 'park.jpg']
+const remontFiles = ['2007%20(1).jpg', '2007%20(2).jpg', '2007%20(4).jpg', '2007%20(5).jpg', '2014%20(1).JPG', '20230716_134946.jpg', '20231011_135743.jpg', '20251019_135913.jpg', '20251214_142137.jpg', 'IMG_0284.jpg', 'IMG_0285-2.jpg', 'IMG_0286-2.jpg', 'IMG_0287.jpg', 'IMG_0288.jpg', 'IMG_0289.jpg', 'IMG_0302.jpg', 'IMG_0304.jpg', 'IMG_0307.jpg', 'IMG_0308.jpg', 'IMG_0312.jpg', 'IMG_0313.jpg', 'IMG_0315.jpg', 'IMG_0316.jpg', 'IMG_0319.jpg', 'IMG_0323.jpg', 'IMG_0325.jpg', 'IMG_0338.jpg', 'IMG_0341.jpg', 'IMG_0346.jpg', 'IMG_0352.jpg', 'IMG_0363.jpg']
+
 const galleryItems = [
-  { src: '/images/roszkowice/gallery/474122627_593199166841747_6658887505205181737_n.jpg', classes: 'col-md-6 col-sm-6 exterior' },
-  { src: '/images/roszkowice/gallery/474162930_593207903507540_3237014207776020479_n.jpg', classes: 'col-md-3 col-sm-3 interior' },
-  { src: '/images/roszkowice/gallery/474204322_593207626840901_4591217624674053052_n.jpg', classes: 'col-md-3 col-sm-3 park' },
-  { src: '/images/roszkowice/gallery/474447454_593207950174202_2676854654486144187_n.jpg', classes: 'col-md-3 col-sm-3 others' },
-  { src: '/images/roszkowice/gallery/474558152_593207893507541_8166201314338965762_n.jpg', classes: 'col-md-3 col-sm-3 exterior' },
-  { src: '/images/roszkowice/gallery/474641782_593199050175092_2384128519064534491_n.jpg', classes: 'col-md-6 col-sm-6 park exterior' }
+  ...zewnFiles.map((file) => ({ src: `/images/roszkowice/zewn/${file}`, classes: 'col-md-3 col-sm-3 exterior' })),
+  ...parkFiles.map((file) => ({ src: `/images/roszkowice/park/${file}`, classes: 'col-md-3 col-sm-3 park' })),
+  ...remontFiles.map((file) => ({ src: `/images/roszkowice/remont/${file}`, classes: 'col-md-3 col-sm-3 remont' }))
 ]
 
 const filters = [
   { filter: '*', labelKey: 'gallery.all', active: true },
   { filter: '.exterior', labelKey: 'gallery.exterior', active: false },
-  { filter: '.interior', labelKey: 'gallery.interior', active: false },
   { filter: '.park', labelKey: 'gallery.park', active: false },
-  { filter: '.others', labelKey: 'gallery.other', active: false }
+  { filter: '.remont', labelKey: 'gallery.remont', active: false }
 ]
 
 function GallerySection({ standalone = false }) {
