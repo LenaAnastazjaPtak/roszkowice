e5717cf36afd9be1cb61d756a802f28646b257d5
Lena Ptak <lena.anastazja.ptak@gmail.com>
2026-03-14 21:58:10 +0100

    feat: about us - background page

--- app/src/pages/AboutPage.jsx
+++ app/src/pages/AboutPage.jsx
@@ -1,31 +1,42 @@
-import { useTranslation } from 'react-i18next'
-import { Link } from 'react-router-dom'
-import YouTubeEmbed from '../components/YouTubeEmbed'
-import PageBanner from '../components/PageBanner'
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
+import YouTubeEmbed from "../components/YouTubeEmbed";
+import PageBanner from "../components/PageBanner";
 
 function AboutPage() {
-  const { t } = useTranslation('about')
+  const { t } = useTranslation("about");
 
   return (
     <>
-      <PageBanner title={t('title')} image="/images/roszkowice/park/IMG_0345.jpg" />
+      <PageBanner
+        title={t("title")}
+        image="/images/roszkowice/park/IMG_0345.jpg"
+      />
       <div className="container-fluid no-padding welcome-section2">
         <div className="container">
           <div className="row">
             <div className="col-md-6 col-sm-6 img-block">
               <i>
-                <img src="/images/roszkowice/logo_with_white_background.png" alt={t('welcome.imgAlt')} style={{ maxWidth: '100%', height: 'auto', objectFit: 'contain' }} />
+                <img
+                  src="/images/roszkowice/logo_with_white_background.png"
+                  alt={t("welcome.imgAlt")}
+                  style={{
+                    maxWidth: "100%",
+                    height: "auto",
+                    objectFit: "contain",
+                  }}
+                />
               </i>
             </div>
             <div className="col-md-6 col-sm-6 content-block">
               <div className="section-header2">
-                <span>{t('welcome.welcomeIn')}</span>
-                <h2>{t('welcome.palaceName')}</h2>
+                <span>{t("welcome.welcomeIn")}</span>
+                <h2>{t("welcome.palaceName")}</h2>
               </div>
-              <p>{t('welcome.paragraph1')}</p>
-              <p>{t('welcome.paragraph2')}</p>
-              <Link to="/gallery" className="btn-link" title={t('seePhotos')}>
-                {t('seePhotos')}
+              <p>{t("welcome.paragraph1")}</p>
+              <p>{t("welcome.paragraph2")}</p>
+              <Link to="/gallery" className="btn-link" title={t("seePhotos")}>
+                {t("seePhotos")}
               </Link>
             </div>
           </div>
@@ -34,24 +45,28 @@ function AboutPage() {
       <div className="container-fluid no-padding onview-section">
         <div className="container">
           <div className="col-md-5 col-sm-5 col-xs-12 img-block">
-            <img src="/images/roszkowice/zewn/pionowe.jpg" alt={t('onview.imgAlt')} style={{ width: '100%', height: 'auto', objectFit: 'cover' }} />
+            <img
+              src="/images/roszkowice/zewn/pionowe.jpg"
+              alt={t("onview.imgAlt")}
+              style={{ width: "100%", height: "auto", objectFit: "cover" }}
+            />
           </div>
           <div className="col-md-7 col-sm-7 col-xs-12 onview-content">
             <div className="section-header">
               <div className="section-title-border">
-                <span>{t('onview.sectionLabel')}</span>
-                <h2>{t('onview.heading')}</h2>
+                <span>{t("onview.sectionLabel")}</span>
+                <h2>{t("onview.heading")}</h2>
               </div>
             </div>
-            <p>{t('onview.paragraph1')}</p>
-            <p>{t('onview.paragraph2')}</p>
-            <p>{t('onview.invite')}</p>
+            <p>{t("onview.paragraph1")}</p>
+            <p>{t("onview.paragraph2")}</p>
+            <p>{t("onview.invite")}</p>
           </div>
         </div>
       </div>
       <YouTubeEmbed url="https://www.youtube.com/watch?v=gPcxPDInHqg" />
     </>
-  )
+  );
 }
 
-export default AboutPage
+export default AboutPage;
