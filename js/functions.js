97bc1c009a5c4f403b2d2392873576c63a7c69b5
Lena Ptak <lena.anastazja.ptak@gmail.com>
2026-03-15 21:52:36 +0100

    refactor: remove Google Map functionality from JavaScript

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,6 @@
 /*
 	Project Name : History
 	
-	-- Google Map
-	
 	## Document Scroll		
 
 	## Document Ready
@@ -16,8 +14,6 @@
 		- Blog Carousel
 		- Lightbox for Highlights Video
 		- Testimonial Slider
-		- Contact Map
-		- Quick Contact Form
 
 	## Window Load
 		- Site Loader
@@ -37,49 +33,6 @@
 			$( "[id='mis_slide_content-"+slideNum+"']" ).addClass("animated fadeIn");
 		}
 	}
-	
-	/* - Google Map */
-	function initialize(obj) {
-		var lat = $('#'+obj).attr("data-lat");
-        var lng = $('#'+obj).attr("data-lng");
-		var contentString = $('#'+obj).attr("data-string");
-		var myLatlng = new google.maps.LatLng(lat,lng);
-		var map, marker, infowindow;
-		var image = "images/marker.png";
-		var zoomLevel = parseInt($('#'+obj).attr("data-zoom") ,10);		
-		var styles = [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}]
-		var styledMap = new google.maps.StyledMapType(styles,{name: "Styled Map"});	
-		
-		var mapOptions = {
-			zoom: zoomLevel,
-			disableDefaultUI: true,
-			center: myLatlng,
-            scrollwheel: false,
-			mapTypeControlOptions: {
-            mapTypeIds: [google.maps.MapTypeId.ROADMAP, "map_style"]
-			}
-		}
-		
-		map = new google.maps.Map(document.getElementById(obj), mapOptions);	
-		
-		map.mapTypes.set("map_style", styledMap);
-		map.setMapTypeId("map_style");
-		
-		infowindow = new google.maps.InfoWindow({
-			content: contentString
-		});      
-	    
-        marker = new google.maps.Marker({
-			position: myLatlng,
-			map: map,
-			icon: image
-		});
-
-		google.maps.event.addListener(marker, "click", function() {
-			infowindow.open(map,marker);
-		});	
-	}
-
 	
 	/* ## Document Scroll - Window Scroll */
 	$( document ).on("scroll",function()
@@ -304,10 +257,6 @@
 			});
 		}
 		
-		/* - Contact Map */
-		if($("#map-canvas-contact").length==1){
-			initialize("map-canvas-contact");
-		}
 		
 		
 	});	/* - Document Ready /- */
